1b8e8dc9385641bfc73d110e23c750a0c6bc54b5
piyush-mahajan <98900169+piyush-mahajan@users.noreply.github.com>
2024-09-01 20:31:22 +0530

    build logs

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  scripts: {
+    build: "[my-framework] build --output public",
+  },
   build: {
     outDir: "build",
   },
